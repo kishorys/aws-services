712fa350039169ea1cdc2285ecee5699ff7dfac7
Kishroa <kishora.ys@tavant.com>
2018-11-21 14:25:53 -0500

    initial commit

---
+++ src/main/java/com/ky/aws/AwsServicesApplication.java
@@ -0,0 +1,12 @@
+package com.ky.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsServicesApplication.class, args);
+	}
+}
